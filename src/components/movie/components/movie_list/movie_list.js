d6ee00849b06654779051d5b03481cc2c5bee5c4
islom-valiyev <islomvalievabudabi@gmail.com>
2023-04-20 20:42:29 +0400

    movie_data qushildi_02

--- src/components/movie/components/movie_list/movie_list.js
+++ src/components/movie/components/movie_list/movie_list.js
@@ -1,18 +1,15 @@
 import './movie_list.css'
 import MovieListItem from '../movie_list_item/movie_list_item'
 
-const MovieList = ({data}) => {
+const MovieList = ({ data }) => {
   return (
     <ul className='movie_list'>
- {/* bunda biz map orqali hamma datalarni olyabmiz */}
+      {/* bunda biz map orqali hamma datalarni olyabmiz */}
       {
-        data.map(item=>(
-      <MovieListItem name = {item.name} viewers = {item.viewers}  favourete ={item.favourete}/>
-      // <MovieListItem{...item} /> bu ikkinichi yul
-
+        data.map(item => (
+          <MovieListItem key={item.id} name={item.name} viewers={item.viewers} favourite={item.favourite} />
         ))
       }
-
     </ul>
   )
 }
