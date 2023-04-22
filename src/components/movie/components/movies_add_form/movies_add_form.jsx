aaa54f8ef8a7d7718cf2bef05b89003877d84a7b
islom-valiyev <islomvalievabudabi@gmail.com>
2023-04-22 12:12:35 +0400

    #16. Formalar bilan ishlash input

--- src/components/movie/components/movies_add_form/movies_add_form.jsx
+++ src/components/movie/components/movies_add_form/movies_add_form.jsx
@@ -1,17 +1,47 @@
 import './movies_add_form.css'
-const moviesAddForm = () => {
-  return (
-    <div className='movies-add-form'>
-      <h3>Yangi kino qushish</h3>
-      <form className='add-form d-flex'>
-        <input type="text" className='form-control new-post-label' placeholder='Qanday kino' />
-        <input type="number" className='form-control new-post-label' placeholder='Nechi marotaba kurilgan'  />
-        <button type="submit" className='btn btn-outline-dark'>
+import { Component } from 'react'
+class moviesAddForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "kino",
+      views: ""
+    }
+  }
+  changeHandele = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render() {
+    const { name, views } = this.state
+    return (
+      <div className='movies-add-form'>
+        <h3>Yangi kino qushish</h3>
+        <form className='add-form d-flex'>
+          <input type="text" className='form-control new-post-label' placeholder='Qanday kino' onChange={this.changeHandele} name='name' value={name} />
+          <input type="number" className='form-control new-post-label' placeholder='Nechi marotaba kurilgan' onChange={this.changeHandele} name='views' value={views} />
+          <button type="submit" className='btn btn-outline-dark'>
             Qushish
-        </button>
-      </form> 
-    </div>
-  )
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
+// const moviesAddForm = () => {
+//   return (
+//     <div className='movies-add-form'>
+//       <h3>Yangi kino qushish</h3>
+//       <form className='add-form d-flex'>
+//         <input type="text" className='form-control new-post-label' placeholder='Qanday kino' />
+//         <input type="number" className='form-control new-post-label' placeholder='Nechi marotaba kurilgan'  />
+//         <button type="submit" className='btn btn-outline-dark'>
+//             Qushish
+//         </button>
+//       </form> 
+//     </div>
+//   )
+// }
 
 export default moviesAddForm
