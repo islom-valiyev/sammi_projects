74a7806e73cac1b262b07184771c287d6abe2272
islom-valiyev <islomvalievabudabi@gmail.com>
2023-04-24 12:01:16 +0400

    #20. Qidiruv komponenti

--- src/components/movie/components/app/App.js
+++ src/components/movie/components/app/App.js
@@ -19,7 +19,8 @@ class App extends Component {
                 { name: "Breaking Bad", viewers: '600', favourite: false, id: 2, like: false },
                 { name: "Spider-Man", viewers: '545', favourite: false, id: 3, like: false },
                 { name: "John", viewers: '785', id: 4, like: false, favourite: false },
-            ]
+            ],
+            term:'',
         };
     }
 
@@ -58,6 +59,17 @@ class App extends Component {
         }))
     }
 
+    searchHandle = (arr, term) => {
+        if (term.length === 0) {
+          return arr;
+        }
+        const lowerCaseTerm = term.toLowerCase();
+        return arr.filter((item) => item.name.toLowerCase().indexOf(lowerCaseTerm) > -1);
+      };
+      
+      UpdateTermHandler=(term)=>{
+        this.setState({term})
+      }
     // Handle deleting a movie by removing the movie with the matching ID from the state
     // onDelate = (id) => {
     //     this.setState(({data}) => {
@@ -73,18 +85,19 @@ class App extends Component {
 
     // Render the main App component
     render() {
-        const { data } = this.state
+        const { data,term } = this.state
         const allMoviesCount = data.length
         const favoriteMoviesCount = data.filter(c => c.favourite === true).length
+        const visibleData =this.searchHandle(data,term)
         return (
             <div className="app font-monospace">
                 <div className="content">
                     <AppInfo allMoviesCount={allMoviesCount} favoriteMoviesCount={favoriteMoviesCount} />
                     <div className='cearch_panel'>
-                        <SearchPanel />
+                        <SearchPanel UpdateTermHandler={this.UpdateTermHandler} />
                         <AppFilter />
                     </div>
-                    <MovieList data={data} onDelate={this.onDelete} onTogleProp={this.onTogleProp} />
+                    <MovieList data={visibleData} onDelate={this.onDelete} onTogleProp={this.onTogleProp} />
                     <MoviesAddForm addForm={this.addForm} />
                 </div>
             </div>
--- src/components/movie/components/search_panel/search_panel.js
+++ src/components/movie/components/search_panel/search_panel.js
@@ -1,8 +1,23 @@
+import { Component } from 'react'
 import './search_panel.css'
-function SearchPanel() {
-  return (
-    <input type="text" className="form-control search-input"  placeholder="kinolarni qidirish"/>
-  )
-}
+class SearchPanel extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      term:'',
+    }
+  }
+  UpdateTermHandler = e =>{
+    const term = e.target.value
+    this.setState({term})
+    this.props.UpdateTermHandler(term)
 
+  }
+
+  render(){
+    return(
+    <input type="text" className="form-control search-input"  placeholder="kinolarni qidirish" onChange={this.UpdateTermHandler} value={this.state.term}/>
+    )
+  }
+}
 export default SearchPanel
