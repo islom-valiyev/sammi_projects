c9af9192b703d374cf8f4097eca0d6d5f111bf20
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-04 22:53:35 +0400

    Update_laSt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from "./components/class_components13/app/app"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
