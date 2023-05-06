e229419c653bbbbab0fcba395a1225e6b4e4283b
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-07 01:18:28 +0400

    update counter function

--- src/components/class_components13/counter/counterFunction.jsx
+++ src/components/class_components13/counter/counterFunction.jsx
@@ -7,17 +7,14 @@ const CounterFunction = () => {
 
     const handleClickIncrease = () => {
         setCount(counter + 1)
-
 }
 
 const handleClickDecrease =() => {
     setCount(counter -1)
-
 }
 
 const handleClickZero = () => {
     setCount(0)
-
 }
 
 
@@ -28,9 +25,12 @@ return (
             <button className="btn btn-success mx-3" onClick={handleClickIncrease}>Increase</button>
             <button className="btn btn-danger mx-3" onClick={handleClickDecrease}>Decrease</button>
             <button className="btn btn-success mx-3" onClick={handleClickZero}>Restart</button>
-
             <p className="counter ">{counter}</p>
         </div>
+        <div className="wrapper">
+            <button></button>
+        </div>
+
     </div>
 )
 }
