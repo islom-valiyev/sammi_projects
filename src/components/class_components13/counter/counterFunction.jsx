2a0b1d1dbabb007157931c0068a36005c2154e96
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-07 01:33:53 +0400

    update counter function_update

--- src/components/class_components13/counter/counterFunction.jsx
+++ src/components/class_components13/counter/counterFunction.jsx
@@ -4,35 +4,42 @@ import { useState } from 'react'
 const CounterFunction = () => {
 
     const [counter, setCount] = useState(0)
+    const [islogin , setIsLogin] = useState(true)
 
     const handleClickIncrease = () => {
-        setCount(counter + 1)
-}
+        setCount(pervState => pervState + 1)
+    }
 
-const handleClickDecrease =() => {
-    setCount(counter -1)
-}
+    const handleClickDecrease = () => {
+        setCount(pervState => pervState - 1)
+    }
 
-const handleClickZero = () => {
-    setCount(0)
-}
+    const handleClickZero = () => {
+        setCount(0)
+    }
+    const onTogleLoading = () => {
+        setIsLogin(prevState => !prevState)
+    }
 
 
-return (
-    <div className="wrapper w-50 mx-auto mb-5" >
-        <h2 className="title">Counter</h2>
-        <div className="counterContainer">
-            <button className="btn btn-success mx-3" onClick={handleClickIncrease}>Increase</button>
-            <button className="btn btn-danger mx-3" onClick={handleClickDecrease}>Decrease</button>
-            <button className="btn btn-success mx-3" onClick={handleClickZero}>Restart</button>
-            <p className="counter ">{counter}</p>
+    return (
+        <div className="wrapper w-50 mx-auto mb-5" >
+            <h2 className="title">Counter</h2>
+            <div className="counterContainer">
+                <button className="btn btn-success mx-3" onClick={handleClickIncrease}>Increase</button>
+                <button className="btn btn-danger mx-3" onClick={handleClickDecrease}>Decrease</button>
+                <button className="btn btn-success mx-3" onClick={handleClickZero}>Restart</button>
+                <p className="counter ">{counter}</p>
+            </div>
+            <div>
+                {islogin? <p className='text-center mt-3' >Login</p> :null}
+                <div className="d-flex justify-content-center">
+                <button className='btn btn-primary ' onClick = {onTogleLoading}> TOGGLE </button>
+                </div>
+            </div>
+
         </div>
-        <div className="wrapper">
-            <button></button>
-        </div>
-
-    </div>
-)
+    )
 }
 
 export default CounterFunction
