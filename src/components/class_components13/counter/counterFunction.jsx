f2b2f9640c42a9ab38204f94d1b43610d37ba902
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-11 03:24:32 +0400

    update

--- src/components/class_components13/counter/counterFunction.jsx
+++ src/components/class_components13/counter/counterFunction.jsx
@@ -1,49 +1,37 @@
-import React, { useState, useMemo } from "react";
-import ReactDOM from "react-dom";
-
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
-  const calculation = useMemo(() => expensiveCalculation(count), [count]);
-
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
-  const addTodo = () => {
-    setTodos((t) => [...t, "New Todo"]);
-  };
-
-  return (
-    <div>
-      <div>
-        <h2>My Todos</h2>
-        {todos.map((todo, index) => {
-          return <p key={index}>{todo}</p>;
-        })}
-        <button onClick={addTodo}>Add Todo</button>
-      </div>
-      <hr />
-      <div>
-        Count: {count}
-        <button onClick={increment}>+</button>
-        <h2>Expensive Calculation</h2>
-        {calculation}
-      </div>
-    </div>
-  );
-};
-
-const expensiveCalculation = (num) => {
-  console.log("Calculating...");
-  for (let i = 0; i < 1000000000; i++) {
-    num += 1;
-  }
-  return num;
-};
+import { useState, useCallback } from 'react'
+import './counter_new.css'
+import CounterItem from './item-generete'
 
 const CounterFunction = () => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<App />);
-};
+    const [counter, setCount] = useState(0)
+    const [active, setActive] = useState(false)
 
-export default CounterFunction;
+    const handleClickIncrease = () => {
+        setCount(prevState => prevState + 1)
+    }
+    const onToggle = () => {
+        setActive(prevState => !prevState)
+    }
+
+    const counterGenerate = useCallback(() => {
+        return new Array(counter).fill(" ").map((_, idx) => ` ${idx=== 0 ?idx+1 : idx} counter number ${idx}`)
+    },[counter])
+
+    const color = {
+        color: active ? 'blue' : 'red'
+    }
+
+    return (
+        <div className="wrapper w-50 mx-auto mb-5">
+            <h2 className="title">Counter</h2>
+            <div className="counterContainer">
+                <button className="btn btn-success mx-3" onClick={handleClickIncrease}>Increase</button>
+                <button className="btn btn-danger mx-3" onClick={onToggle}>Toggle</button>
+                <p className="counter" style={color}>{counter}</p>
+            </div>
+            <CounterItem counterGenerate={counterGenerate} />
+        </div>
+    )
+}
+
+export default CounterFunction
