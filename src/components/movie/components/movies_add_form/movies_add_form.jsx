946766fb1e01e8494ee2d4ca8a5ead444e4a9da8
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-13 04:42:36 +0400

    #30. Loyihani hooklarga o'girish

--- src/components/movie/components/movies_add_form/movies_add_form.jsx
+++ src/components/movie/components/movies_add_form/movies_add_form.jsx
@@ -1,72 +1,45 @@
-import './movies_add_form.css'
-import { Component } from 'react'
+import './movies_add_form.css';
+import { useState } from 'react';
 
-// Create a new component called moviesAddForm which extends the Component class
-class moviesAddForm extends Component {
-  // Constructor function which initializes the component's state
-  constructor(props) {
-    super(props)
-    // Set the component's initial state to an object with two properties: 'name' with an empty string as its value, and 'views' with an empty string as its value
-    this.state = {
-      name: "",
-      views: ""
-    }
-  }
+const MoviesAddForm = ({ addForm }) => {
+	const [state, setState] = useState({ name: '', views: '' })
 
-  // Function which is called whenever the value of an input field is changed
-  changeHandele = (e) => {
-    // Update the component's state to include the new value of the input field, using the input field's 'name' attribute as the key
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+	const changeHandlerInput = e => setState({ ...state, [e.target.name]: e.target.value })
 
-  // Function which is called when the form is submitted
-  addFormHandle = e => {
-    e.preventDefault()
-    // Call the parent component's addForm function and pass it an object with the 'name' and 'views' properties set to the current values of the component's state
-    this.props.addForm({ name: this.state.name, viewers:this.state.views, })
-    // Reset the component's state to its initial values
-    this.setState({
-      name: '',
-      views: ''
-    })
-  }
+	const addFormHandler = e => {
+		e.preventDefault()
+		if (state.name === '' || state.views === '') return
+		const data = { name: state.name, viewers: state.views }
+		addForm(data)
+		setState({ name: '', views: '' })
+	}
 
-  // Render function which generates the component's HTML
-  render() {
-    const { name, views } = this.state
-    return (
-      <div className='movies-add-form'>
-        <h3>Yangi kino qushish</h3>
-        <form className='add-form d-flex' onSubmit={this.addFormHandle}>
-          <input type="text" className='form-control new-post-label' placeholder='Qanday kino' onChange={this.changeHandele} name='name' value={name} />
-          <input type="number" className='form-control new-post-label' placeholder='Nechi marotaba kurilgan' onChange={this.changeHandele} name='views' value={views} />
-          <button type="submit" className='btn btn-outline-dark'>
-            Qushish
-          </button>
-        </form>
-      </div>
-    )
-  }
+	return (
+		<div className='movies-add-form'>
+			<h3>Yangi kino qo'shish</h3>
+			<form className='add-form d-flex' onSubmit={addFormHandler}>
+				<input
+					type='text'
+					className='form-control new-post-label'
+					placeholder='Qanday kino?'
+					onChange={changeHandlerInput}
+					name='name'
+					value={state.name}
+				/>
+				<input
+					type='number'
+					className='form-control new-post-label'
+					placeholder="Nechi marotaba ko'rilgan?"
+					onChange={changeHandlerInput}
+					name='views'
+					value={state.views}
+				/>
+				<button type='submit' className='btn btn-outline-dark'>
+					Qo'shish
+				</button>
+			</form>
+		</div>
+	)
 }
 
-// Export the moviesAddForm component as the default export of this module
-export default moviesAddForm
-
-// const moviesAddForm = () => {
-//   return (
-//     <div className='movies-add-form'>
-//       <h3>Yangi kino qushish</h3>
-//       <form className='add-form d-flex'>
-//         <input type="text" className='form-control new-post-label' placeholder='Qanday kino' />
-//         <input type="number" className='form-control new-post-label' placeholder='Nechi marotaba kurilgan'  />
-//         <button type="submit" className='btn btn-outline-dark'>
-//             Qushish
-//         </button>
-//       </form> 
-//     </div>
-//   )
-// }
-
-// export default moviesAddForm
+export default MoviesAddForm
--- src/components/movie/components/search_panel/search_panel.js
+++ src/components/movie/components/search_panel/search_panel.js
@@ -1,34 +1,24 @@
-import { Component } from 'react' 
+import { useState } from 'react' 
 import './search_panel.css' 
 // SearchPanel nomli yangi komponent yaratiladi, bu Component klassidan extends qilinadi
-class SearchPanel extends Component {
-  // Komponentning holatini boshlash uchun constructor funksiyasi
-  constructor(props) {
-    super(props)
-    // Komponentning boshlang'ich holatini tekshirishda foydalaniladigan 'term' nomli bo'sh satr qiymatini o'z ichiga olgan obyekt sifatida o'rnating
-    this.state = {
-      term: '',
-    }
-  }
+const SearchPanel = props => {
+	const [term, setTerm] = useState('')
 
-  // Input maydoni qiymati o'zgartirilganda chaqiriladigan funksiya
-  UpdateTermHandler = e => {
-    // Input maydonining qiymatini olish
-    const term = e.target.value
-    // Komponentning holatini yangilab, yangi qiymatni qo'shish
-    this.setState({term})
-    // Ota komponentning UpdateTermHandler funksiyasini chaqirib, yangi qiymatni o'z ichiga oladi
-    this.props.UpdateTermHandler(term)
-  }
+	const updateTermHandler = e => {
+		const term = e.target.value.toLowerCase()
+		setTerm(term)
+		props.updateTermHandler(term)
+	}
 
-  // Komponentning HTML kodini yaratish uchun render funksiyasi
-  render() {
-    return(
-      // Xususiy klass nomi va placeholder matni bilan input maydoni yaratiladi. Qiymati komponentning 'term' holatining joriy qiymati bo'ladi va qiymati o'zgartirilganda UpdateTermHandler funksiyasi chaqiriladi
-      <input type="text" className="form-control search-input" placeholder="kinolarni qidirish" onChange={this.UpdateTermHandler} value={this.state.term}/>
-    )
-  }
+	return (
+		<input
+			type='text'
+			className='form-control search-input'
+			placeholder='Kinolarni qidirish'
+			onChange={updateTermHandler}
+			value={term}
+		/>
+	)
 }
 
-// SearchPanel komponentini ushbu modulning standart eksporti sifatida eksport qilingan
 export default SearchPanel
