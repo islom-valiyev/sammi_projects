663eaddc5e491bea49cd5509431ef443bdbb56b4
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-17 14:26:21 +0400

    updated

--- sammitube/src/components/app/app.jsx
+++ sammitube/src/components/app/app.jsx
@@ -1,7 +1,12 @@
+import { Stack,Button } from "@mui/material";
 function App() {
     return (
         <div className="App">
-            <h1>hello</h1>
+            <Stack spacing={2} direction="row">
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Stack>
         </div>
     );
 }
