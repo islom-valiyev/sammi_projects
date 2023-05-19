c4d3e63f531f98c75c75c0d43640782f194395fa
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-19 17:05:31 +0400

    updated

--- sammitube/src/components/category/category.jsx
+++ sammitube/src/components/category/category.jsx
@@ -1,16 +1,16 @@
 import { Stack } from "@mui/material"
 import { category } from '../../constants/index'
 import { colors } from "../../constants/colors"
-const Category = () => {
+const Category = ({ selactedCategoryHandle, selactedCategory }) => {
     return (
         <Stack direction={'row'} sx={{ height: 'auto', overflowX: 'scroll ' }}>
             {category.map(item => (
-                <button key={item.name} className="category-btn" style={{ borderRadius: '0' }}>
-                    <span style={{color:colors.secondary , marginRight:'15px' }}>{item.icon}</span>
-                    <span style={{opacity: '1',}}>{item.name}</span>
+                <button key={item.name} className="category-btn" style={{ borderRadius: '0', backgroundColor: item.name === selactedCategory && colors.secondary, color: item.name === selactedCategory && 'white' }} onClick={() => selactedCategoryHandle(item.name)}>
+                    <span style={{ color: colors.secondary, marginRight: '15px' }}>{item.icon}</span>
+                    <span style={{ opacity: '1', }}>{item.name}</span>
                 </button>
-    ))
-}
+            ))
+            }
         </Stack >
     )
 }
--- sammitube/src/components/main/main.jsx
+++ sammitube/src/components/main/main.jsx
@@ -4,15 +4,16 @@ import { Stack, Box, Container, Typography, } from "@mui/material"
 import {Category} from '../index'
 const Main = () => {
   const [selactedCategory,setSelactedCategory] = useState('new')
+  const selactedCategoryHandle =category =>setSelactedCategory(category)
   return (
     <Stack>
-      <Category/>
+      <Category selactedCategoryHandle ={selactedCategoryHandle} selactedCategory={selactedCategory} />
       <Box p={2} sx={{ height: '90vh' }}>
         <Container maxWidth={'90%'} >
           <Typography variant={"h4"} fontWeight={'bold'} mb={2} >
             {selactedCategory} <span style={{color: colors.secondary}}>videos</span>
           </Typography>
-          Videos
+          Videoss
         </Container>
       </Box>
     </Stack>
