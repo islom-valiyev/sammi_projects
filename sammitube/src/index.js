6ecb5c42786f695348fb490209f11d6bc8028d16
islom-valiyev <islomvalievabudabi@gmail.com>
2023-05-20 12:25:41 +0400

    #43. Video card

--- sammitube/src/index.js
+++ sammitube/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components';
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
