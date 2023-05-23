3660f3738e1fde211e46c88781dad6b1d7f6fbab
Islom_valiev <92217377+islom-valiyev@users.noreply.github.com>
2023-05-23 07:24:20 +0000

    updated

--- sammitube/src/components/main/main.jsx
+++ sammitube/src/components/main/main.jsx
@@ -5,37 +5,37 @@ import { Category, Videos } from "../index";
 import { ApiService } from '../../service/api.service'
 const Main = () => {
 
-  const [selactedCategory, setSelactedCategory] = useState("New");
+	const [selactedCategory, setSelactedCategory] = useState("New");
 
-  const selactedCategoryHandle = (category) => setSelactedCategory(category);
-  const [videos, setVideos] = useState([])
+	const selactedCategoryHandle = (category) => setSelactedCategory(category);
+	const [videos, setVideos] = useState([])
 
-  // console.log(process.env.REACT_APP_PUBLIC_KEY);
+	// console.log(process.env.REACT_APP_PUBLIC_KEY);
 
-  useEffect(() => {
-    ApiService.fetching(`search?part=snippet&q=${selactedCategory}`).then(data => setVideos(data.items))
-  }, [selactedCategory])
+	useEffect(() => {
+		ApiService.fetching(`search?part=snippet&q=${selactedCategory}`).then(data => setVideos(data.items))
+	}, [selactedCategory])
 
-  return (
-    <Stack>
-      <Category
-        selactedCategoryHandle={selactedCategoryHandle}
-        selactedCategory={selactedCategory}
-      />
-      <Box p={2} sx={{ height: "90vh" }}>
-        <Container maxWidth={"90%"}>
-          <Typography variant={"h4"} fontWeight={"bold"} mb={2}>
-            {selactedCategory}{" "}
-            <span style={{ color: colors.secondary }}>videos</span>
-          </Typography>
-          <Videos videos={videos} />
-          {videos.items ? videos.items.map(c => c.kind) : 'loading...'}
-          {/* {videos.items.map(c => c.kind)} */}
+	return (
+		<Stack>
+			<Category
+				selactedCategoryHandle={selactedCategoryHandle}
+				selactedCategory={selactedCategory}
+			/>
+			<Box p={2} sx={{ height: "90vh" }}>
+				<Container maxWidth={"90%"}>
+					<Typography variant={"h4"} fontWeight={"bold"} mb={2}>
+						{selactedCategory}{" "}
+						<span style={{ color: colors.secondary }}>videos</span>
+					</Typography>
+					<Videos videos={videos} />
+					{videos.items ? videos.items.map(c => c.kind) : 'loading...'}
+					{/* {videos.items.map(c => c.kind)} */}
 
-        </Container>
-      </Box>
-    </Stack>
-  );
+				</Container>
+			</Box>
+		</Stack>
+	);
 };
 
 export default Main;
