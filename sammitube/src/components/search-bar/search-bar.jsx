c172d4842f368eb7a799b16406980865d2044e1e
Islom_valiev <92217377+islom-valiyev@users.noreply.github.com>
2023-05-23 09:39:55 +0000

    #45. Search page

--- sammitube/src/components/search-bar/search-bar.jsx
+++ sammitube/src/components/search-bar/search-bar.jsx
@@ -1,15 +1,37 @@
-import { Search } from '@mui/icons-material'
-import { colors } from '../../constants/colors'
-import { Paper, IconButton } from '@mui/material'
-const SearchBar = () => {
-    return (
-        <Paper component={'form'} sx={{ border:`1px solid ${colors.primary}`, pl:2, boxShadow: "none" }}>
-            <input type="text" placeholder='Search...'className='search-bar' />
-            <IconButton>
-                <Search />
-            </IconButton>
-        </Paper>
-    )
-}
+import { Search } from "@mui/icons-material";
+import { colors } from "../../constants/colors";
+import { Paper, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom ";
+import { useState } from "react";
 
-export default SearchBar
+
+const SearchBar = () => {
+const navigate = useNavigate()
+	const [value, setValue] = useState("");
+	const submitHandler = (e) => {
+		e.preventDefault();
+		if (value) {
+			navigate(`/search/${value}`)
+		}
+	};
+	return (
+		<Paper
+			component={"form"}
+			sx={{ border: `1px solid ${colors.primary}`, pl: 2, boxShadow: "none" }}
+			onSubmit={submitHandler}
+		>
+			<input
+				type="text"
+				placeholder="Search..."
+				className="search-bar"
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<IconButton type="submit">
+				<Search />
+			</IconButton>
+		</Paper>
+	);
+};
+
+export default SearchBar;
