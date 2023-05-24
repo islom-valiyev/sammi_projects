c54b4c43cdbbf769325df8849564a7442c6f8c9d
Islom Valiev <ivaliev@lab2r4s11.42abudhabi.ae>
2023-05-24 13:48:01 +0400

    update

--- sammitube/src/components/search-bar/search-bar.jsx
+++ sammitube/src/components/search-bar/search-bar.jsx
@@ -1,12 +1,12 @@
 import { Search } from "@mui/icons-material";
 import { colors } from "../../constants/colors";
 import { Paper, IconButton } from "@mui/material";
-import { useNavigate } from "react-router-dom ";
 import { useState } from "react";
+import {useNavigate} from "react-router-dom"
 
 
 const SearchBar = () => {
-const navigate = useNavigate()
+	const navigate = useNavigate()
 	const [value, setValue] = useState("");
 	const submitHandler = (e) => {
 		e.preventDefault();
