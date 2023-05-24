aaa5865f058b3eb1c5ff4b7d8a877a1d109bbf8a
islom-valiyev <islomvalievabudabi>
2023-05-24 15:50:04 +0400

    update 45

--- sammitube/src/components/videos/videos.jsx
+++ sammitube/src/components/videos/videos.jsx
@@ -5,12 +5,10 @@ const Videos = ({ videos }) => {
 
 	return (
 		<Stack width={'100 %'} direction={'row'} flexWrap={'wrap'} justifyContent={'start'} alignItems={'center'} gap={2}>
-			{videos.map(item => (
-			
+			{videos.map(item => (			
 				<Box key={item.id.videoId} >
 					{item.id.videoId && <VideoCard video={item} />}
 					{item.id.channeLId && <ChannelCard video={item} />}
-
 				</Box>
 			))} 
 		</Stack>
