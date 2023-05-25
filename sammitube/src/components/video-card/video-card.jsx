42eb8511538dffa81a9987027825c7959164b8b4
islom-valiyev <islomvalievabudabi>
2023-05-25 15:35:28 +0400

    #46. Video detail page

--- sammitube/src/components/video-card/video-card.jsx
+++ sammitube/src/components/video-card/video-card.jsx
@@ -2,6 +2,7 @@ import { Avatar, Card, CardContent, CardMedia, Stack, Typography, } from "@mui/m
 import { colors } from "../../constants/colors"
 import moment from 'moment'
 import { CheckCircle } from "@mui/icons-material";
+import{Link} from "react-router-dom"
 
 const VideoCard = ({ video }) => {
 
@@ -9,9 +10,11 @@ const VideoCard = ({ video }) => {
 
     return (
         <Card sx={{ width: { xs: '100%', sm: '360px', md: '320px' }, boxShadow: 'none', borderRadius: 0 }}>
+			<Link to={`/video/${video.id.videoId}`}>
             <CardMedia image={video?.snippet?.thumbnails?.high?.url} alt={video?.snippet?.title} sx={{ width: {xs:'100%' , sm :'360px', md:'320px'}, height: '180px' }} />
+			</Link>
             <CardContent sx={{ background: colors.primary, height: '200px', position: 'relative' }}>
-                <>
+                <Link to={`/video/${video.id.videoId}`}>
                     <Typography my={'5'} sx={{ opacity: '4' }}>
                         {moment(video?.snippet?.publishedAt).fromNow()}
                     </Typography>
@@ -20,7 +23,7 @@ const VideoCard = ({ video }) => {
                         {video?.snippet?.title.slice(0, 50)}
                     </Typography>
                     <Typography variant="subtitle1" sx={{ opacity: '.6' }}> {video?.snippet?.description.slice(0, 50)}</Typography>
-                </>
+                </Link>
                 <>
                     <Stack direction={'row'} position={'absolute'} bottom={'10px'} alignItems={'center'} gap={'5px'}>
                         <Avatar src={video?.snippet?.thumbnails?.high?.url} />
--- sammitube/src/components/video-detail/video-detail.jsx
+++ sammitube/src/components/video-detail/video-detail.jsx
@@ -1,9 +1,93 @@
+import { Avatar, Box, Chip, Stack, Typography } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { ApiService } from '../../service/api.service'
+import ReactPlayer from 'react-player'
+import { CheckCircle, FavoriteOutlined, MarkChatRead, Tag, Visibility } from '@mui/icons-material'
+// import renderHTML from 'react-render-html'
+import { Loader } from '../'
 const VideoDetail = () => {
-    return (
-        <div>
+	const [videoDetail, setvideoDetail] = useState(null)
+	// console.log(videoDetail);
 
-        </div>
-    )
+	const { id } = useParams()
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const data = await ApiService.fetching(`videos?part=snippet,statistics&id=${id}`)
+				setvideoDetail(data.items[0])
+				console.log(data);
+			} catch (error) {
+				console.log(error);
+			}
+
+		}
+		getData()
+	}, [id])
+	if(!videoDetail?.snippet) return <Loader/>
+	// const { snippet: { title, channelId, channelTitle, description, tags, thumnails },
+	// 	statistics: { viewCount, likeCount, commentCount },
+	// } = VideoDetail
+	return (
+		<Box minHeight={'90vh'} mb={10} >
+			<Box display={'flex'}>
+			<Box width={{ xs: '100%', md: '75%' }}>
+					<ReactPlayer
+						url={`https://www.youtube.com/watch?v=${id}`}
+						className='react-player'
+						controls
+					/>
+					{videoDetail?.snippet.tags.map((item, idx) => (
+						<Chip
+							label={item}
+							key={idx}
+							sx={{ marginTop: '10px', cursor: 'pointer', ml: '10px' }}
+							deleteIcon={<Tag />}
+							onDelete={() => {}}
+							variant='outlined'
+						/>
+					))}
+					<Typography variant='h5' fontWeight='bold' p={2}>
+						{videoDetail.snippet.title}
+					</Typography>
+					<Typography variant='subtitle2' p={2} sx={{ opacity: '.7' }}>
+					{/* <Typography> { videoDetail.snippet.description}</Typography>						 */}
+					</Typography>
+					<Stack direction='row' gap='20px' alignItems='center' py={1} px={2}>
+						<Stack sx={{ opacity: 0.7 }} direction='row' alignItems='center' gap='3px'>
+							<Visibility />
+							{parseInt(videoDetail.statistics.viewCount).toLocaleString()} views
+						</Stack>
+						<Stack sx={{ opacity: 0.7 }} direction='row' alignItems='center' gap='3px'>
+							<FavoriteOutlined />
+							{parseInt(videoDetail.statistics.likeCount).toLocaleString()} likes
+						</Stack>
+						<Stack sx={{ opacity: 0.7 }} direction='row' alignItems='center' gap='3px'>
+							<MarkChatRead />
+							{parseInt(videoDetail.statistics.commentCount).toLocaleString()} comment
+						</Stack>
+					</Stack>
+					<Stack direction='row' py={1} px={2}>
+						<Link to={`/channel/${videoDetail?.snippet?.channelId}`}>
+							<Stack direction='row' alignItems='center' gap='5px' marginTop='5px'>
+								<Avatar
+									alt={videoDetail.snippet.channelTitle}
+									src={videoDetail.snippet.thumbnails.default.url}
+								/>
+								<Typography variant='subtitle2' color='gray'>
+									{videoDetail.snippet.channelTitle}
+									<CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
+								</Typography>
+							</Stack>
+						</Link>
+					</Stack>
+				</Box>
+				<Box width={'25%'} > video suggested</Box>
+			</Box>
+
+		</Box>
+	)
 }
 
 export default VideoDetail
