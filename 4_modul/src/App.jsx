68bc83f2c8255114965c31ea28aab93961eb82f2
islom-valiyev <islomvalievabudabi>
2023-05-30 04:14:03 +0400

    #56. Navbar komponent

--- 4_modul/src/App.jsx
+++ 4_modul/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom"
-import { Main, Login, Register } from "./components/index"
+import { Main, Login, Register, Navbar } from "./components/index"
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
--- 4_modul/src/components/index.js
+++ 4_modul/src/components/index.js
@@ -1,5 +1,7 @@
 export { default as Main } from './main'
 export { default as Login } from './login.jsx'
 export { default as Register } from './register'
+export { default as Navbar } from './navbar'
+
 
 
