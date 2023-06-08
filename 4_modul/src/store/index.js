1b4411607e64095ec4f11b8d1fd77e142db14658
islom-valiyev <islomvalievabudabi>
2023-06-08 14:33:51 +0400

    #58. Redux. Redux toolkit

--- 4_modul/src/store/index.js
+++ 4_modul/src/store/index.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { AuthSlice } from '../slice/auth'
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+    AuthSlice,
+  },
+  devTools: process.env.NODE_ENV !== 'prodection',
 })  
---
+++ 4_modul/src/ui/input.jsx
@@ -0,0 +1,10 @@
+const Input = ({ label, type = 'text', setState }) => {
+    return (
+        <div>  <div class="form-floating">
+            <input type={type} onChange={e => setState(e.target.value)} class="form-control" id="floatingInput" placeholder={label} />
+            <label for="floatingInput">{label}</label>
+        </div></div>
+    )
+}
+
+export default Input
