c66258723b87066a2d854b24d855f9b5234cd685
islom-valiyev <islomvalievabudabi>
2023-06-11 03:05:03 +0400

    updated input value for clean

--- 4_modul/src/ui/input.jsx
+++ 4_modul/src/ui/input.jsx
@@ -1,7 +1,7 @@
-const Input = ({ label, type = 'text', setState }) => {
+const Input = ({ label, type = 'text', value, setState }) => {
     return (
         <div>  <div className="form-floating">
-            <input type={type} onChange={e => setState(e.target.value)} className="form-control" id="floatingInput" placeholder={label} />
+            <input type={type} onChange={e => setState(e.target.value)} value={value} className="form-control" id="floatingInput" placeholder={label} />
             <label htmlFor="floatingInput">{label}</label>
         </div></div>
     )
