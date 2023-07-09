6cc3775e91329b4a51bd06ac7667334d7fd427a7
Islom_valiev <92217377+islom-valiyev@users.noreply.github.com>
2023-07-09 12:08:39 +0500

    Update input.jsx

--- 4_modul/src/ui/input.jsx
+++ 4_modul/src/ui/input.jsx
@@ -1,1 +1,11 @@
 
+const Input = ({ label, type = 'text', state, setState }) => {
+    return (
+        <div>  <div className="form-floating">
+            <input type={type} onChange={e => setState(e.target.value)} value={state} className="form-control" id="floatingInput" placeholder={label} />
+            <label htmlFor="floatingInput">{label}</label>
+        </div></div>
+    )
+}
+
+export default Input
